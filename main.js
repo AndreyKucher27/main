586d31d7d71cfccb5ba157988e7f6bcd8ed0abca
AndreyKucher27 <144955355+AndreyKucher27@users.noreply.github.com>
2023-11-20 22:50:29 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function itogl() {
+    const productSelect = document.getElementById("product");
+    const kolvoInput = document.getElementById("kolvo");
+    const totalSpan = document.getElementById("total");
+    const prices = { product1: 200, product2: 300, product3: 400 };
+    const selectedProduct = productSelect.value;
+    const kolvoValue = kolvoInput.value;
+    if (!isNaN(kolvoValue) && kolvoValue > 0) { 
+        const price = prices[selectedProduct];
+        const kolvo = kolvoInput.value;
+        const total = price * kolvo;
+        totalSpan.textContent = total + " ₽";
+    } 
+    else {
+        totalSpan.textContent = "Ошибка ввода";
+    }
+}
+window.addEventListener("DOMContentLoaded", function (event) {
+    const calculateButton = document.getElementById("calculate");
+    calculateButton.addEventListener("click", itogl);
+});
