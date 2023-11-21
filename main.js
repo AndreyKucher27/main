efb418a2aa14ed05f6e9086c6ba090e85d432638
AndreyKucher27 <144955355+AndreyKucher27@users.noreply.github.com>
2023-11-22 00:11:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-function itogl() {
-    const productSelect = document.getElementById("product");
-    const kolvoInput = document.getElementById("kolvo");
-    const totalSpan = document.getElementById("total");
-    const prices = { product1: 200, product2: 300, product3: 400 };
-    const selectedProduct = productSelect.value;
-    const kolvoValue = kolvoInput.value;
-    if (!isNaN(kolvoValue) && kolvoValue > 0) { 
-        const price = prices[selectedProduct];
-        const kolvo = kolvoInput.value;
-        const total = price * kolvo;
-        totalSpan.textContent = total + " ₽";
+function c() {
+    const productCh = document.getElementById("product"); /*сохраняем html объект в эту переменную, выбор продукта*/
+    const kolvoT = document.getElementById("kolvo"); /*сохраняем html объект в эту переменную, кол-во товара, нужно нам*/
+    const totalS = document.getElementById("total"); /*сохраняем html объект в эту переменную, итоговая сумма, результат*/
+    const prices = { product1: 1450, product2: 900, product3: 550 };
+    const selectedProduct = productCh.value; /*запоминаем значение выбранного товара*/
+    const kolvoValue = kolvoT.value; /*запоминаем значение количества товара*/
+    if (!isNaN(kolvoValue) && kolvoValue > 0) { /*условие проверяет введенное нами значение на число и >0*/
+        const price = prices[selectedProduct]; /*Ставим цену выбранного товара */
+        const kolvo = kolvoT.value;/*ставим количество выбранного товара */
+        const total = price * kolvo; /*получаем сумму заказа */
+        totalS.textContent = total + " ₽"; /*возвращает строку с полученной суммой заказа */
     } 
     else {
-        totalSpan.textContent = "Ошибка ввода";
+        totalS.textContent = "Ошибка ввода"; /*возвращает ошибку в ином случае */
     }
 }
 window.addEventListener("DOMContentLoaded", function (event) {
     const calculateButton = document.getElementById("calculate");
-    calculateButton.addEventListener("click", itogl);
+    calculateButton.addEventListener("click", c);
 });
